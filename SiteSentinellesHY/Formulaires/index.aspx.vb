214c2b7cc903e380c9dc7c0cf2ea25f8d9662604
Matvallee <matvallee@gmail.com>
2014-02-07 08:40:08 -0500

    enlever commentaire

--- SiteSentinellesHY/Formulaires/index.aspx.vb
+++ SiteSentinellesHY/Formulaires/index.aspx.vb
@@ -75,11 +75,6 @@ Public Class index
         If listeRevuesDePresse.Count > 0 Then
             listeRevuesDePresse.RemoveAt(0)
         End If
-<<<<<<< .mine
-        'Mathieu
-=======
-        'Mathieu 
->>>>>>> .theirs
         Return listeRevuesDePresse.AsQueryable
     End Function
 #End Region
